333fb4186750b369fcd1762e8fa9796c7f6e699e
Marcos Canales <marketes94@gmail.com>
2017-10-08 20:00:27 +0200

    vertex2normal

--- kfusion/src/opencl/common_opencl.cpp
+++ kfusion/src/opencl/common_opencl.cpp
@@ -17,7 +17,7 @@
 #define STR(x) XSTR(x)
 
 #ifndef AOCX_PATH
-#define AOCX_PATH "/home/mcanales/Desktop/slambench/kfusion/src/opencl/kernels_fpga_v1"
+#define AOCX_PATH "/home/mcanales/Desktop/slambench/kfusion/src/opencl/kernels_fpga_vertex2normal_de5net"
 #endif
 
 cl_int             clError;
