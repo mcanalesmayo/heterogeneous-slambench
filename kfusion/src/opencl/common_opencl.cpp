14220e4d4c66bc7f01f8aadd3e4c38dddcbacede
Marcos Canales <marketes94@gmail.com>
2017-10-08 20:32:07 +0200

    renderTrack

--- kfusion/src/opencl/common_opencl.cpp
+++ kfusion/src/opencl/common_opencl.cpp
@@ -17,7 +17,7 @@
 #define STR(x) XSTR(x)
 
 #ifndef AOCX_PATH
-#define AOCX_PATH "/home/root/heterogeneous-slambench/kfusion/src/opencl/kernels_fpga_renderTrack"
+#define AOCX_PATH "/home/mcanales/Desktop/slambench/kfusion/src/opencl/kernels_fpga_renderTrack_de5net"
 #endif
 
 cl_int             clError;
