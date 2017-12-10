2a83afb1b05721e7cf792cdef276905ff4e29928
Marcos Canales <marketes94@gmail.com>
2017-12-10 18:58:00 +0100

    original GPU vertex2normal

--- kfusion/src/opencl/kernels.cpp
+++ kfusion/src/opencl/kernels.cpp
@@ -43,6 +43,23 @@
 
 #endif
 
+inline double benchmark_tock() {
+	synchroniseDevices();
+#ifdef __APPLE__
+	clock_serv_t cclock;
+	mach_timespec_t clockData;
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
+	clock_get_time(cclock, &clockData);
+	mach_port_deallocate(mach_task_self(), cclock);
+#else
+	struct timespec clockData;
+	clock_gettime(CLOCK_MONOTONIC, &clockData);
+#endif
+	return (double) clockData.tv_sec + clockData.tv_nsec / 1000000000.0;
+}
+
+double startOfKernel, endOfKernel;
+
 cl_kernel vertex2normal_ocl_kernel;
 
 cl_mem * ocl_inputNormal = NULL;
@@ -93,14 +110,13 @@ void Kfusion::languageSpecificConstructor() {
     ocl_inputNormal = (cl_mem*) malloc(sizeof(cl_mem) * iterations.size());
 
 	for (unsigned int i = 0; i < iterations.size(); ++i) {
-		printf("%d * (%d * %d) / (%d) sizeof buffer: %d\n", sizeof(float), computationSize.x, computationSize.y, (int) pow(2, i), sizeof(float) * (computationSize.x * computationSize.y) / (int) pow(2, i));
-		ocl_inputVertex[i] = clCreateBuffer(contexts[0], CL_MEM_READ_WRITE, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), NULL, &clError);
+		ocl_inputVertex[i] = clCreateBuffer(contexts[1], CL_MEM_READ_WRITE, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), NULL, &clError);
 		checkErr(clError, "clCreateBuffer");
-        ocl_inputNormal[i] = clCreateBuffer(contexts[0], CL_MEM_READ_WRITE, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), NULL, &clError);
+        ocl_inputNormal[i] = clCreateBuffer(contexts[1], CL_MEM_READ_WRITE, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), NULL, &clError);
         checkErr(clError, "clCreateBuffer");
 	}
 
-	vertex2normal_ocl_kernel = clCreateKernel(programs[0], "vertex2normalKernel", &clError);
+	vertex2normal_ocl_kernel = clCreateKernel(programs[1], "vertex2normalKernel", &clError);
 	checkErr(clError, "clCreateKernel");
 
 	if (getenv("KERNEL_TIMINGS"))
@@ -292,7 +308,7 @@ void vertex2normalKernel(uint2 imageSize, int i) {
 
     size_t globalWorksize2[2] = { imageSize.x, imageSize.y };
 
-    clError = clEnqueueNDRangeKernel(cmd_queues[0][0], vertex2normal_ocl_kernel, 2, NULL, globalWorksize2, NULL, 0, NULL, NULL);
+    clError = clEnqueueNDRangeKernel(cmd_queues[1][0], vertex2normal_ocl_kernel, 2, NULL, globalWorksize2, NULL, 0, NULL, NULL);
     checkErr(clError, "clEnqueueNDRangeKernel");
 
 	//TOCK("vertex2normalKernel", imageSize.x * imageSize.y);
@@ -976,17 +992,22 @@ bool Kfusion::tracking(float4 k, float icp_threshold, uint tracking_rate,
 						computationSize.y / (int) pow(2, i - 1)), e_delta * 3, 1);
 	}
 
+	timings[5] = 0.0f;
+
 	// prepare the 3D information from the input depth maps
 	uint2 localimagesize = computationSize;
 	for (unsigned int i = 0; i < iterations.size(); ++i) {
 		Matrix4 invK = getInverseCameraMatrix(k / float(1 << i));
 		depth2vertexKernel(inputVertex[i], ScaledDepth[i], localimagesize, invK);
 
-        clError = clEnqueueWriteBuffer(cmd_queues[0][0], ocl_inputVertex[i], CL_TRUE, 0, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), &inputVertex[i][0], 0, NULL, NULL);
+		startOfKernel = benchmark_tock();
+        clError = clEnqueueWriteBuffer(cmd_queues[1][0], ocl_inputVertex[i], CL_TRUE, 0, sizeof(float3) * (computationSize.x * computationSize.y) / (int) pow(2, i), &inputVertex[i][0], 0, NULL, NULL);
         checkErr(clError, "clEnqueueWriteBuffer");
 
 		vertex2normalKernel(localimagesize, i);
 		localimagesize = make_uint2(localimagesize.x / 2, localimagesize.y / 2);
+		endOfKernel = benchmark_tock();
+		timings[5] += endOfKernel - startOfKernel;
 	}
 
 	oldPose = pose;
@@ -1101,7 +1122,7 @@ void Kfusion::computeFrame(const ushort * inputDepth, const uint2 inputSize,
 
 
 void synchroniseDevices() {
-	clFinish(cmd_queues[0][0]);
+	clFinish(cmd_queues[1][0]);
 	//clFinish(cmd_queues[1][0]);
 	//clFinish(cmd_queues[1][1]);
 }
