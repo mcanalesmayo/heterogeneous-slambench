bfb3fe3c7172aadced7b6920c8401f1f419bbf7f
Marcos Canales <marketes94@gmail.com>
2017-12-27 00:20:45 +0100

    original FPGA reduce

--- kfusion/src/opencl/kernels.cpp
+++ kfusion/src/opencl/kernels.cpp
@@ -43,6 +43,25 @@
 
 #endif
 
+inline double benchmark_tock() {
+	synchroniseDevices();
+#ifdef __APPLE__
+	clock_serv_t cclock;
+	mach_timespec_t clockData;
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
+	clock_get_time(cclock, &clockData);
+	mach_port_deallocate(mach_task_self(), cclock);
+#else
+	struct timespec clockData;
+	clock_gettime(CLOCK_MONOTONIC, &clockData);
+#endif
+	return (double) clockData.tv_sec + clockData.tv_nsec / 1000000000.0;
+}
+
+double startOfKernel, endOfKernel;
+
+bool updatePoseKernelRes;
+
 cl_kernel reduce_ocl_kernel;
 
 cl_mem ocl_reduce_output_buffer = NULL;
@@ -984,6 +1003,8 @@ bool Kfusion::tracking(float4 k, float icp_threshold, uint tracking_rate,
 	oldPose = pose;
 	const Matrix4 projectReference = getCameraMatrix(k) * inverse(raycastPose);
 
+	timings[7] = 0.0f;
+
 	for (int level = iterations.size() - 1; level >= 0; --level) {
 		uint2 localimagesize = make_uint2(
 				computationSize.x / (int) pow(2, level),
@@ -994,7 +1015,7 @@ bool Kfusion::tracking(float4 k, float icp_threshold, uint tracking_rate,
 					localimagesize, vertex, normal, computationSize, pose,
 					projectReference, dist_threshold, normal_threshold);
 
-
+			startOfKernel = benchmark_tock();
 
 			clEnqueueWriteBuffer(cmd_queues[0][0], ocl_trackingResult, CL_TRUE, 0, sizeof(TrackData) * (computationSize.x * computationSize.y), trackingResult, 0, NULL, NULL);
 			checkErr(clError, "clEnqueueReadBuffer");
@@ -1036,11 +1057,11 @@ bool Kfusion::tracking(float4 k, float icp_threshold, uint tracking_rate,
 
 
 
-			/*reduceKernel(reductionoutput, trackingResult, computationSize,
-					localimagesize);*/
+			updatePoseKernelRes = updatePoseKernel(pose, reductionoutput, icp_threshold);
+			endOfKernel = benchmark_tock();
+			timings[7] += endOfKernel - startOfKernel;
 
-			if (updatePoseKernel(pose, reductionoutput, icp_threshold))
-				break;
+			if (updatePoseKernelRes) break;
 
 		}
 	}
