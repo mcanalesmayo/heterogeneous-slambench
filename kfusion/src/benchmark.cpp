2a498aca3846be4bdad25c3bfa9a837bf2c24299
Marcos Canales <marketes94@gmail.com>
2018-05-20 10:47:10 +0200

    fixed kfusion obj creation

--- kfusion/src/benchmark.cpp
+++ kfusion/src/benchmark.cpp
@@ -144,10 +144,9 @@ int main(int argc, char ** argv) {
 	double* timingsIO = (double *) malloc(13 * sizeof(double));
 	double* timingsCPU = (double *) malloc(13 * sizeof(double));
 	double* timingsCustom = (double *) calloc(256, sizeof(double));
-    double* timingsBuffers = (double *) calloc(13, sizeof(double));
 	double startOfKernel, endOfKernel, computationTotalIO, computationTotalCPU, computationTotalCustom, overallTotalIO, overallTotalCPU, overallTotalCustom;
 	Kfusion kfusion(computationSize, config.volume_resolution,
-			config.volume_size, init_pose, config.pyramid, timingsIO, timingsCPU, logstreamCustom);
+			config.volume_size, init_pose, config.pyramid, timingsIO, timingsCPU, logstreamCustom, logstreamBuffers);
 
 	*logstreamIO
 			<< "frame\tacquisition\tpreprocess_mm2meters\tpreprocess_bilateralFilter\ttrack_halfSample\ttrack_depth2vertex\ttrack_vertex2normal"
